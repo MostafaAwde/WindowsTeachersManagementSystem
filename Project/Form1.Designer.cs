4e7aff98a7e38efd67c0305dedd2141f465bf630
MostafaAwde <82030084@students.liu.edu.lb>
2024-03-08 13:43:27 +0200

    first commit

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Project
+{
+    partial class LecturesManagementSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClassesLectures = new System.Windows.Forms.Button();
+            this.btnClasses = new System.Windows.Forms.Button();
+            this.btnSubjects = new System.Windows.Forms.Button();
+            this.btnTeachers = new System.Windows.Forms.Button();
+            this.btnLectures = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.btnClassesLectures);
+            this.panel1.Controls.Add(this.btnClasses);
+            this.panel1.Controls.Add(this.btnSubjects);
+            this.panel1.Controls.Add(this.btnTeachers);
+            this.panel1.Controls.Add(this.btnLectures);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnExit.Location = new System.Drawing.Point(0, 535);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(186, 42);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClassesLectures
+            // 
+            this.btnClassesLectures.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnClassesLectures.FlatAppearance.BorderSize = 0;
+            this.btnClassesLectures.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClassesLectures.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClassesLectures.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnClassesLectures.Location = new System.Drawing.Point(0, 312);
+            this.btnClassesLectures.Name = "btnClassesLectures";
+            this.btnClassesLectures.Size = new System.Drawing.Size(186, 42);
+            this.btnClassesLectures.TabIndex = 4;
+            this.btnClassesLectures.Text = "Classes Lectures";
+            this.btnClassesLectures.UseVisualStyleBackColor = true;
+            this.btnClassesLectures.Click += new System.EventHandler(this.btnClassesLectures_Click);
+            // 
+            // btnClasses
+            // 
+            this.btnClasses.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnClasses.FlatAppearance.BorderSize = 0;
+            this.btnClasses.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClasses.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClasses.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnClasses.Location = new System.Drawing.Point(0, 270);
+            this.btnClasses.Name = "btnClasses";
+            this.btnClasses.Size = new System.Drawing.Size(186, 42);
+            this.btnClasses.TabIndex = 7;
+            this.btnClasses.Text = "Classes";
+            this.btnClasses.UseVisualStyleBackColor = true;
+            this.btnClasses.Click += new System.EventHandler(this.btnClasses_Click);
+            // 
+            // btnSubjects
+            // 
+            this.btnSubjects.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSubjects.FlatAppearance.BorderSize = 0;
+            this.btnSubjects.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubjects.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubjects.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnSubjects.Location = new System.Drawing.Point(0, 228);
+            this.btnSubjects.Name = "btnSubjects";
+            this.btnSubjects.Size = new System.Drawing.Size(186, 42);
+            this.btnSubjects.TabIndex = 6;
+            this.btnSubjects.Text = "Subjects";
+            this.btnSubjects.UseVisualStyleBackColor = true;
+            this.btnSubjects.Click += new System.EventHandler(this.btnSubjects_Click);
+            // 
+            // btnTeachers
+            // 
+            this.btnTeachers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTeachers.FlatAppearance.BorderSize = 0;
+            this.btnTeachers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeachers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeachers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnTeachers.Location = new System.Drawing.Point(0, 186);
+            this.btnTeachers.Name = "btnTeachers";
+            this.btnTeachers.Size = new System.Drawing.Size(186, 42);
+            this.btnTeachers.TabIndex = 5;
+            this.btnTeachers.Text = "Teachers";
+            this.btnTeachers.UseVisualStyleBackColor = true;
+            this.btnTeachers.Click += new System.EventHandler(this.btnTeachers_Click);
+            // 
+            // btnLectures
+            // 
+            this.btnLectures.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLectures.FlatAppearance.BorderSize = 0;
+            this.btnLectures.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLectures.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLectures.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnLectures.Location = new System.Drawing.Point(0, 144);
+            this.btnLectures.Name = "btnLectures";
+            this.btnLectures.Size = new System.Drawing.Size(186, 42);
+            this.btnLectures.TabIndex = 1;
+            this.btnLectures.Text = "Lectures";
+            this.btnLectures.UseVisualStyleBackColor = true;
+            this.btnLectures.Click += new System.EventHandler(this.btnLectures_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label1.Location = new System.Drawing.Point(48, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Manager";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Project.Properties.Resources.Circled_User_Icon_transparent_PNG___StickPNG;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(214, 75);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(686, 356);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnSave.Location = new System.Drawing.Point(800, 455);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 40);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnDelete.Location = new System.Drawing.Point(214, 455);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 40);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Row";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // LecturesManagementSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(929, 577);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LecturesManagementSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Lectures Management System";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClassesLectures;
+        private System.Windows.Forms.Button btnClasses;
+        private System.Windows.Forms.Button btnSubjects;
+        private System.Windows.Forms.Button btnTeachers;
+        private System.Windows.Forms.Button btnLectures;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class LecturesManagementSystem : Form
+    {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        private DataTable dataTable;
+        public LecturesManagementSystem()
+        {
+            InitializeComponent();
+
+            conn = new SqlConnection("Data source=(local);Initial Catalog=Project; Integrated Security=true");
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            dataTable = new DataTable();
+        }
+
+
+        private void LoadData(string tableName)
+        {
+            try
+            {
+                dataTable.Clear();
+                DataTable emptyTable = new DataTable();
+                dataGridView.DataSource = emptyTable;
+                dataTable.Rows.Clear();
+                dataTable.Columns.Clear();
+
+
+                string query = $"SELECT * FROM {tableName}";
+
+                conn.Open();
+
+                cmd.CommandText = query;
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                dataTable.Load(reader);
+
+                reader.Close();
+
+                dataGridView.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnLectures_Click(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = true;
+            string tableName = "Lectures";
+            LoadData(tableName);
+            dataGridView.Tag = tableName;
+        }
+
+        private void btnTeachers_Click(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = true;
+            string tableName = "Teachers";
+            LoadData(tableName);
+            dataGridView.Tag = tableName;
+        }
+
+        private void btnSubjects_Click(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = true;
+            string tableName = "Subjects";
+            LoadData(tableName);
+            dataGridView.Tag = tableName;
+        }
+
+        private void btnClasses_Click(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = false;
+            string tableName = "Classes";
+            LoadData(tableName);
+            dataGridView.Tag = tableName;
+
+        }
+
+        private void btnClassesLectures_Click(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = true;
+            string tableName = "ClassLectures";
+            LoadData(tableName);
+            dataGridView.Tag = tableName;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                DataTable changes = dataTable.GetChanges();
+
+                string tableName = dataGridView.Tag.ToString();
+
+                if (tableName == "ClassLectures")
+                {
+                    foreach (DataRow row in changes.Rows)
+                    {
+                        int classId = Convert.ToInt32(row["ClassId"]);
+                        int lectureId = Convert.ToInt32(row["LectureId"]);
+                        string days = row["Days"].ToString();
+                        TimeSpan startTime = TimeSpan.Parse(row["StartTime"].ToString());
+                        TimeSpan endTime = TimeSpan.Parse(row["EndTime"].ToString());
+
+                        // Check if the class lecture already exists
+                        string checkQuery = $"SELECT COUNT(*) FROM ClassLectures WHERE ClassId = {classId} " +
+                                            $"AND LectureId = {lectureId} AND Days = '{days}' " +
+                                            $"AND StartTime = '{startTime}' AND EndTime = '{endTime}'";
+
+                        SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+                        int count = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                            MessageBox.Show("This class lecture already exists.");
+                            return;
+                        }
+                    }
+                }
+
+                if (tableName == "Lectures")
+                {
+                    foreach (DataRow row in changes.Rows)
+                    {
+                        int teacherId = Convert.ToInt32(row["TeacherId"]);
+                        string days = row["Days"].ToString();
+
+                        // Check if the teacher's available day matches the lecture day
+                        string checkQuery = $"SELECT COUNT(*) FROM Teachers WHERE TeacherId = {teacherId} " +
+                                            $"AND AvailableDays = '{days}'";
+
+                        SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+                        int count = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                        if (count == 0)
+                        {
+                            MessageBox.Show("Teacher can have lectures only on their available days.");
+                            return;
+                        }
+                    }
+                }
+
+
+                if (changes != null)
+                {
+                    string query = $"SELECT * FROM {tableName}";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+                    // Configure the adapter to handle insert operations
+                    builder.GetInsertCommand();
+
+                    // Update the database with the changes
+                    adapter.Update(changes);
+
+                    // Refresh the DataTable to reflect the changes from the database
+                    dataTable.Clear();
+                    adapter.Fill(dataTable);
+                }
+
+                MessageBox.Show("Data saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedCells.Count > 0)
+            {
+                try
+                {
+                    // Get the selected cell
+                    DataGridViewCell selectedCell = dataGridView.SelectedCells[0];
+
+                    // Get the corresponding row
+                    DataGridViewRow selectedRow = selectedCell.OwningRow;
+
+                    // Get the currently active table name from the Tag property
+                    string tableName = dataGridView.Tag.ToString();
+                    int id = 0;
+                    string subjectName = string.Empty;
+
+                    if (tableName == "Teachers")
+                    {
+                        // Get the primary key value from the selected row
+                        id = Convert.ToInt32(selectedRow.Cells["TeacherId"].Value);
+                    }
+                    else if (tableName == "Lectures")
+                    {
+                        id = Convert.ToInt32(selectedRow.Cells["LectureId"].Value);
+                    }
+                    else if (tableName == "ClassLectures")
+                    {
+                        id = Convert.ToInt32(selectedRow.Cells["ClassLectureId"].Value);
+                    }
+                    else if (tableName == "Subjects")
+                    {
+                        subjectName = selectedRow.Cells["SubjectName"].Value.ToString();
+                    }
+
+                    // Delete the corresponding record from the database
+                    string query = string.Empty;
+                    if (tableName == "Lectures")
+                    {
+                        // Check if the lecture is added to ClassLectures
+                        SqlCommand checkCmd = new SqlCommand($"SELECT COUNT(*) FROM ClassLectures WHERE LectureId = @id", conn);
+                        checkCmd.Parameters.AddWithValue("@id", id);
+                        conn.Open();
+                        int count = (int)checkCmd.ExecuteScalar();
+                        conn.Close();
+
+                        if (count > 0)
+                        {
+                            MessageBox.Show("This lecture is added to ClassLectures and cannot be deleted until it is removed from there.");
+                            return; // Abort the deletion process
+                        }
+
+                        query = $"DELETE FROM {tableName} WHERE LectureId = @id";
+                    }
+                    else if (tableName == "Teachers")
+                    {
+                        query = $"DELETE FROM {tableName} WHERE TeacherId = @id";
+                    }
+                    else if (tableName == "Subjects")
+                    {
+                        query = $"DELETE FROM {tableName} WHERE SubjectName = @subjectName";
+                    }
+                    else if (tableName == "ClassLectures")
+                    {
+                        query = $"DELETE FROM {tableName} WHERE ClassLectureId = @id";
+                    }
+
+                    SqlCommand deleteCmd = new SqlCommand(query, conn);
+                    conn.Open();
+
+                    if (tableName == "Teachers" || tableName == "Lectures" || tableName == "ClassLectures")
+                    {
+                        deleteCmd.Parameters.AddWithValue("@id", id);
+                    }
+                    else if (tableName == "Subjects")
+                    {
+                        deleteCmd.Parameters.AddWithValue("@subjectName", subjectName);
+                    }
+
+                    deleteCmd.ExecuteNonQuery();
+                    conn.Close();
+
+
+                    // Remove the row from the DataGridView
+                    dataGridView.Rows.Remove(selectedRow);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
